d480be29e1441af649b9a50ae8e830c378848e0a
mlatinov <latinovmetodi@gmail.com>
2026-03-18 14:30:34 +0200

    update the main py with new models and evaluation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 from functions.eda_f import eda
 from functions.process_data_f import process_data
 from functions.tune_models_f import *
+from functions.helpf import *
 
 ## Load the data ##
 egg_data = pd.read_csv("data/Egg_Production.csv")
@@ -18,6 +19,12 @@ processing_output = process_data(data=egg_data, Y="Total_egg_production")
 
 ## Tune models ##
 rf_model = tune_rf(processing_output = processing_output, n_trials=10) # Random Forest model
-knn_model = tune_knn(processing_output=processing_output,n_trials=20) # KNN
-# svm_model =
-# gb_model =
+knn_model = tune_knn(processing_output= processing_output,n_trials=20) # KNN
+svm_model = svm_tune(processing_output = processing_output, n_trials= 20) # SVM Model
+hist_boost_model = hist_boost_tune(processing_output = processing_output, n_trails=20) # Histboost
+
+## Predict with the  models on the testing data and evaluate them
+models_evaluation = evaluate_models(
+    models=[rf_model["model"],knn_model["model"],svm_model["model"],hist_boost_model["model"]],
+    processing_output= processing_output
+)
